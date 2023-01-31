cea08a5fc359ea71c65879fafa183805fbfe38bd
Julio Patiño <franjp132@gmail.com>
2023-01-31 16:39:10 -0500

    Trabajando en el componente Categorys

--- src/components/Categorys/Categorys.jsx
+++ src/components/Categorys/Categorys.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { categorys } from '../../mock/mockData';
 
 const Categorys = () => {
   return (
     <div>
-      <h3 className='mt-5 text-3xl text-gray-600 font-light'>Categorías Populares</h3>
-      <div className='bg-white p-5 mt-5 '>
-        Categorys📖
+      <h3 className='mt-5 text-3xl text-gray-600 font-light'>Categorías populares</h3>
+      <div className='bg-white mt-5 grid grid-flow-row grid-cols-3'>
+        {categorys.map(({ name, icon }, index) => (
+          <div className='flex flex-row'>
+            <div key={index} className='border border-solid w-full border-gray-400 flex flex-col items-center justify-center hover:cursor-pointer hover:text-white hover:bg-blue-600 p-7'>
+              <span class="material-symbols-outlined text-blue-600 hover:text-white text-7xl">
+                {icon}
+              </span>
+              <p>{name}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/mock/mockData.js
@@ -0,0 +1,28 @@
+const categorys = [
+  {
+    name: "Celulares y Telefonía",
+    icon:"phone_android"
+  },
+  {
+    name: "Computación",
+    icon:"computer"
+  },
+  {
+    name: "Construcción",
+    icon:"engineering"
+  },
+  {
+    name: "Coleccionables",
+    icon:"album"
+  },
+  {
+    name: "Consolas y Videojuegos",
+    icon:"stadia_controller"
+  },
+  {
+    name: "Camaras y Accesorios",
+    icon:"photo_camera"
+  },
+];
+
+export {categorys}
